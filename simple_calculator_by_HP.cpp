8a0b631a804a84f17487bffc2d47008da795abff
Harshit Pant <133621508+Harshitpant12@users.noreply.github.com>
2024-10-09 19:33:00 +0530

    Add files via upload
    A simple calculator to perform basic arithmetic operations.

---
+++ simple_calculator_by_HP.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int calculate(int n1, int n2, char operand)
+{
+    switch (operand)
+    {
+    case '+':
+        cout << "Addition will be: " << n1 + n2 << endl;
+        break;
+    case '-':
+        cout << "Subtraction will be: " << n1 - n2 << endl;
+        break;
+    case '*':
+        cout << "Multiplication will be: " << n1 * n2 << endl;
+        break;
+    case '/':
+        cout << "Division will be: " << n1 / n2 << endl;
+        break;
+    default:
+        cout << "Please Enter a valid operator." << endl;
+    }
+}
+
+int main()
+{
+    cout << "A calculator for simple calculations -->" << endl;
+    long long int n1, n2;
+    cout << "Enter the first number: " << endl;
+    cin >> n1;
+    cout << "Enter the second number: " << endl;
+    cin >> n2;
+    char op;
+    cout << "Choose an operator between : +, -, *, /" << endl;
+    cout << "Enter the operator: " << endl;
+    cin >> op;
+
+    switch (op)
+    {
+    case '+':
+        cout << "Addition will be: " << n1 + n2 << endl;
+        break;
+    case '-':
+        cout << "Subtraction will be: " << n1 - n2 << endl;
+        break;
+    case '*':
+        cout << "Multiplication will be: " << float(n1) * float(n2) << endl;
+        break;
+    case '/':
+        cout << "Division will be: " << float(n1) / float(n2) << endl;
+        break;
+    default:
+        cout << "Please Enter a valid operator." << endl;
+    }
+
+    return 0;
+}
