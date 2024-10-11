616eda2690430c084f2365889ca3f4288495705f
Harshit Pant <133621508+Harshitpant12@users.noreply.github.com>
2024-10-11 17:40:40 +0530

    Add files via upload

---
+++ number_guessing_game_by_HP.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    srand((unsigned int)time(NULL));
+    int number = (rand() % 100) + 1;
+
+    int guess;
+
+    while (guess != number)
+    {
+        cout << "Guess a number between 1 to 100: " << endl;
+        cin >> guess;
+
+        if (guess > number)
+        {
+            cout << "Your guess is higher, guess a low number." << endl;
+        }
+        else if (guess < number)
+        {
+            cout << "Your guess is lower, guess a high number." << endl;
+        }
+        else
+        {
+            cout << "You got it, You WON!!!" << endl;
+        }
+    }
+
+    return 0;
+}
